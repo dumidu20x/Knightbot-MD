5fdd85b2c92d5da244f0b5b118b380e49cd482c0
dumidu20x <dumiduchathuranga19484@gmail.com>
2025-03-07 21:37:39 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botOwner: 'Dumidu', // Your name
+  ownerNumber: '94710628554', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
